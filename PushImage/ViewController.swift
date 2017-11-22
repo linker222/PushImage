80c449e7d0f0e5f945a09d2b8f4c6c933e2a8b74
shing <shing.linker@gmail.com>
2017-11-22 15:02:18 +0800

    Initial Commit

---
+++ PushImage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PushImage
+//
+//  Created by 施宏政 on 2017/11/22.
+//  Copyright © 2017年 施宏政. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
